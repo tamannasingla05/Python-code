270b944ebb81f4f2521e62671c6a8e31ce0a53a0
Tamanna Singla <tamannasingla2000@gmail.com>
2025-08-29 14:17:36 +0530

    searchInSinglyLL

---
+++ Linked List/01.SinglyLL/2.searching.py
@@ -0,0 +1,28 @@
+#program to search an element in the LL
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+#function to search element
+def search(head,key):
+    curr = head
+
+    while curr is not None:
+        if (curr.data == key):
+            return True
+        
+        curr = curr.next
+
+    return False
+
+if __name__=="__main__":
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+
+    key = 4
+
+    print(search(head,key))
